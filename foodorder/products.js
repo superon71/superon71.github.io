f3183d59967b92a19bde4087d48837f1a46e34e9
superon71 <82932180+superon71@users.noreply.github.com>
2022-06-05 15:51:34 +0800

    multiple sites

---
+++ foodorder/products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 0,
+    name: "California Fish Taco",
+    price: 29.99,
+    cusine : "USA",
+    description:
+      "The classic fish taco—grilled or fried white fish (such as cod or mahi-mahi)",
+    imgSrc: "./img/t1.png",
+  },
+  {
+    id: 1,
+    name: "Mushroom Risotto",
+    price: 24.99,
+    cusine: "Italian",
+    description:
+      "A plateful of buttery risotto with the goodness of mushrooms. ",
+    imgSrc: "./img/t2.png",
+  },
+  {
+    id: 2,
+    name: "Bruschetta",
+    price: 19.99,
+    cusine : "Italian",
+    description:
+      "An antipasto dish, bruschetta has grilled bread topped with veggies, rubbed garlic and tomato mix. ",
+    imgSrc: "./img/t3.png",
+  },
+  {
+    id: 3,
+    name: "Kung Pao Chicken",
+    price: 25.99,
+    cusine : "Chinese",
+    description:
+      "Kung Pao Chicken (宫保鸡丁 gōngbào jīdīng) is a famous Sichuan-style specialty, popular with both Chinese and foreigners.",
+    imgSrc: "./img/t4.png",
+  },
+  {
+    id: 4,
+    name: "Imperial Sweet and Sour Pork",
+    price: 29.99,
+    cusine: "Chinese",
+    description:
+      "Sweet and sour pork (糖醋里脊 tángcù lǐjǐ) has a bright orange-red color, and a delicious sweet and sour taste.",
+    imgSrc: "./img/t5.png",
+  },
+  {
+    id: 5,
+    name: "Dumpling",
+    price: 15.99,
+    cusines: "Chinese",
+    description:
+      "Dumplings (饺子 jiǎozi) are a traditional food type that is widely popular, especially in North China.",
+    imgSrc: "./img/t6.png",
+  },
+];
